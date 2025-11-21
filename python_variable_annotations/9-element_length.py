5dc261a216f909b1e2615352582670cfb3437e57
Fantasar <lapique.philippe@gmail.com>
2025-11-21 16:48:59 +0100

    Ajout des import

--- python_variable_annotations/9-element_length.py
+++ python_variable_annotations/9-element_length.py
@@ -8,7 +8,7 @@ lists de tuples
 from typing import Iterable, Sequence, List, Tuple
 
 
-def element_lenght(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
 
     """
     Fonction qui retourne une listes de tuples
