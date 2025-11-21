bea2029895eb3f1230b2413ff3625c8ca6c4408f
Fantasar <lapique.philippe@gmail.com>
2025-11-21 16:10:03 +0100

    Sauvegardes des fichiers pour test check

---
+++ python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+from typing import Callable
+
+"""
+Module qui permet de réaliser une multiplication
+avec un nombre flottant.
+"""
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+
+    """
+    Fonctions qui prends la valeurs de la variable
+    multiplier et retourne le resultat.
+    """
+
+    def multiply(value: float) -> float:
+
+        """
+        Fonctions pour multiplier une 
+        valeur par le multiplicateur.
+        """
+
+        return value * multiplier
+
+    return multiply
