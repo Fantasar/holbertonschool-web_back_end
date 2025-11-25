93c8dc1ae0b8e611d4dcd05f2afcc23b31e03622
Fantasar <lapique.philippe@gmail.com>
2025-11-25 16:11:31 +0100

    Ajout du dossier et du l'exercice 1 sur la commande asyncio.sleep

---
+++ python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+"""
+Module Async qui prends une valeurs aléatoires pour
+l'attente de la fonction.
+"""
+
+import random
+import asyncio
+
+async def wait_random(max_delay: int = 10) -> float:
+
+    """
+    Fonction qui prends une valeurs aléatoire
+    entre 0 et max_delay.
+    """
+
+    delay = random.uniform(0, max_delay)
+    await asyncio.sleep(delay)
+    return delay
