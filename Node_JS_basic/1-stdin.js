323c59eb0ccb8db60f432dd842c3852372622d96
Fantasar <lapique.philippe@gmail.com>
2026-01-08 12:51:34 +0100

    Modification de la condition de fermeture

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,7 +1,8 @@
+process.stdin.setEncoding('utf8');
 console.log("Welcome to Holberton School, what is your name?");
+
 process.stdin.on('data', (data) => {
-    const name = data.toString().trim();
-    console.log(`Your name is: ${name}`);
+    process.stdout.write(`Your name is: ${data}`);
 });
 
 process.stdin.on('end', () => {
