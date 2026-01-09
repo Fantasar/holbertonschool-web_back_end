838d9e90aca304ae9980a1c01ff6939a60391e71
Fantasar <lapique.philippe@gmail.com>
2026-01-09 15:40:47 +0100

    fix: Semistandard

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,10 +1,11 @@
-process.stdin.setEncoding('utf8');
-console.log("Welcome to Holberton School, what is your name?");
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
+
+process.stdin.setEncoding('utf-8');
 
 process.stdin.on('data', (data) => {
-    process.stdout.write(`Your name is: ${data}`);
+  process.stdout.write(`Your name is: ${data}`);
 });
 
 process.stdin.on('end', () => {
-    console.log("This important software is now closing");
+  process.stdout.write('This important software is now closing\n');
 });
