36dfefd559b9bbcd1fa03e83fd3dfc93632d5536
Fantasar <lapique.philippe@gmail.com>
2026-01-09 16:34:25 +0100

    Modification Semistandard

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -1,67 +1,54 @@
 const http = require('http');
 const fs = require('fs');
 
-function countStudents (path) {
-  return new Promise((resolve, reject) => {
-    fs.readFile(path, 'utf-8', (err, data) => {
-      if (err) {
-        reject(new Error('Cannot load the database'));
-      } else {
-        const lines = data.split('\n').filter((line) => line.trim() !== '');
+const countStudents = (path) => new Promise((resolve, reject) => {
+  fs.readFile(path, 'utf-8', (error, data) => {
+    if (error) {
+      reject(new Error('Cannot load the database'));
+      return;
+    }
 
-        if (lines.length <= 1) {
-          reject(new Error('Cannot load the database'));
-          return;
+    const lines = data.split('\n').filter((line) => line.trim() !== '');
+    const students = lines.slice(1);
+    const output = [];
+
+    output.push(`Number of students: ${students.length}`);
+
+    const fields = {};
+    students.forEach((line) => {
+      const [firstname, , , field] = line.split(',');
+      if (firstname && field) {
+        if (!fields[field]) {
+          fields[field] = [];
         }
-
-        const students = lines.slice(1);
-        const fields = {};
-
-        students.forEach((student) => {
-          const [firstname, , , field] = student.split(',');
-          if (firstname && field) {
-            if (!fields[field]) {
-              fields[field] = [];
-            }
-            fields[field].push(firstname);
-          }
-        });
-
-        let result = `Number of students: ${students.length}\n`;
-
-        // Trier les fields alphabétiquement
-        const sortedFields = Object.keys(fields).sort((a, b) => a.localeCompare(b));
-
-        sortedFields.forEach((field) => {
-          result += `Number of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}\n`;
-        });
-
-        resolve(result);
+        fields[field].push(firstname);
       }
     });
+
+    for (const [field, names] of Object.entries(fields)) {
+      output.push(`Number of students in ${field}: ${names.length}. List: ${names.join(', ')}`);
+    }
+
+    resolve(output.join('\n'));
   });
-}
+});
 
 const app = http.createServer(async (req, res) => {
   res.setHeader('Content-Type', 'text/plain');
 
   if (req.url === '/') {
-    res.statusCode = 200;
     res.end('Hello Holberton School!');
   } else if (req.url === '/students') {
-    const databasePath = process.argv[2];
-
-    try {
-      const result = await countStudents(databasePath);
-      res.statusCode = 200;
-      res.end(`This is the list of our students\n${result}`);
-    } catch (error) {
-      res.statusCode = 500;
-      res.end(`This is the list of our students\n${error.message}`);
-    }
+    const database = process.argv[2];
+    countStudents(database)
+      .then((output) => {
+        res.end(`This is the list of our students\n${output}`);
+      })
+      .catch(() => {
+        res.end('This is the list of our students\nCannot load the database');
+      });
   } else {
-    res.statusCode = 404;
-    res.end();
+    res.end('Hello Holberton School!');
   }
 });
 
